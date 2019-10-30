edf665d264b62cc8a2a09218611c305930e5154f
hoiril mochtar <horimo.me@gmail.com>
2019-10-30 11:11:34 +0700

    menambahkan file kedalam project

---
+++ src/projectpbo2/tampungBelanja.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectpbo2;
+
+/**
+ *
+ * @author anonymous
+ */
+public class tampungBelanja {
+    public static int baris = 0;
+    public static String PRODUK[] = new String[999999];
+    public static int JUMLAH[] = new int[999999];
+    public static int TOTALJUMLAH;
+    public static String HARGA[] = new String[999999];
+    public static int TOTALHARGA;
+}
---
+++ src/projectpbo2/tampungMember.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectpbo2;
+
+/**
+ *
+ * @author anonymous
+ */
+public class tampungMember {
+    public static int baris = 0;
+    public static String ID_PASIEN[] = new String[999999];
+    public static String NAMA[] = new String[999999];
+    public static String ALAMAT[] = new String[999999];
+    public static String NO_TLPN[] = new String[999999];
+    public static String TANGGAL[] = new String[999999];
+    public static String PAKET[] = new String[999999];
+    public static String PASSWORD[] = new String[999999];
+}
